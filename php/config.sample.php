f1b829021e1aefdde030d5f58c6c2ace022e9d1f
floppy69 <61385774+floppy69@users.noreply.github.com>
2022-03-21 21:31:49 +0100

    Update config.sample.php

--- php/config.sample.php
+++ php/config.sample.php
@@ -1,88 +1,73 @@
 <?php
 /**
- * Bitcoin Status Page
+ * Dogecoin Status Page
  *
  * @category File
- * @package  BitcoinStatus
+ * @package  DogecoinStatus
  * @author   Craig Watson <craig@cwatson.org>
+ * @Forked By   Floppy69 <a.maaded@gmail.com>
  * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
- * @link     https://github.com/craigwatson/bitcoind-status
+ * @link     https://github.com/floppy69/dogecoind-status
  */
 
 $config = array(
   // RPC
-  'rpc_user'                  => 'rpcuser',
-  'rpc_pass'                  => 'rpcpass',
-  'rpc_host'                  => 'localhost',
-  'rpc_port'                  => '8332',
-  'rpc_ssl'                   => false,
-  'rpc_ssl_ca'                => null,
+  'rpc_user'                => 'rpcuser',
+  'rpc_pass'                => 'rpcpasswd',
+  'rpc_host'                => 'localhost',
+  'rpc_port'                => 'rpcport',
+  'rpc_ssl'                 => false,
+  'rpc_ssl_ca'              => null,
 
-  //Node Port for bitnodes
-  'node_port'                 => 8333,
-  
   // Donations
-  'display_donation_text'     => false,
-  'donation_address'          => 'not_set',
-  'donation_amount'           => '0.001',
+  'display_donation_text'   => true,
+  // 'donation_address'        => 'DNxB12TG8ijZxcG4oGaDAgVNuzSxdkMRDD', // Mine Dogenation address
+  'donation_address'        => 'not_set',
+  'donation_amount'         => '1.69420', 
 
   // Peers
-  'display_peer_info'         => false,
-  'display_peer_port'         => false,
-  'hide_dark_peers'           => true,
-  'ignore_unknown_ping'       => false,
-  'peers_to_ignore'           => array(),
+  'display_peer_info'       => true,
+  'display_peer_port'       => false,
+  'hide_dark_peers'         => true,
+  'peers_to_ignore'         => array(),
 
   // Cache
-  'cache_geo_data'            => false,
-  'geo_cache_file'            => '/var/tmp/bitcoind-geolocation.cache',
-  'geo_cache_time'            => 604800,
-  'use_cache'                 => true,
-  'cache_file'                => '/tmp/bitcoind-status.cache',
-  'max_cache_time'            => 300,
-  'nocache_whitelist'         => array('127.0.0.1'),
+  'use_cache'               => true,
+  'cache_file'              => '/tmp/dogecoind-status.cache',
+  'max_cache_time'          => 60,
+  'nocache_whitelist'       => array('127.0.0.1'),
 
   // Geolocation
-  'geolocate_peer_ip'         => false,
-  'display_ip_location'       => false,
+  'geolocate_peer_ip'       => true,
+  'display_ip_location'     => true,
 
   // UI
-  'display_ip'                => false,
-  'display_free_disk_space'   => false,
-  'display_testnet'           => false,
-  'display_version'           => true,
-  'display_github_ribbon'     => true,
-  'display_max_height'        => true,
-  'use_bitcoind_ip'           => true,
-  'intro_text'                => 'not_set',
-  'title_text'                => 'Bitcoin Node Status',
-  'display_bitnodes_info'     => false,
-  'display_chart'             => false,
-  'display_peer_chart'        => false,
-  'node_links'                => array(),
+  'display_ip'              => true,
+  'display_free_disk_space' => false,
+  'display_testnet'         => false,
+  'display_version'         => true,
+  'display_github_ribbon'   => true,
+  'display_max_height'      => true,
+  'use_dogecoind_ip'         => true,
+  'intro_text'              => 'Dogecoin node - Much WoW !',
+  'display_sochain_info'   => true,
+  'display_chart'           => true,
+  'node_links'              => array(),
 
   // Stats
-  'stats_whitelist'           => array('127.0.0.1'),
-  'stats_file'                => '/tmp/bitcoind-status.data',
-  'stats_max_age'             => '604800',
-  'stats_min_data_points'     => 5,
-
-  // Node Count
-  'peercount_whitelist'       => array('127.0.0.1'),
-  'peercount_file'            => '/tmp/bitcoind-peers.data',
-  'peercount_max_age'         => '2592000',
-  'peercount_min_data_points' => 10,
-  'peercount_extra_nodes'     => array(),
+  'stats_whitelist'         => array('127.0.0.1'),
+  'stats_file'              => '/tmp/dogecoind-status.data',
+  'stats_max_age'           => '604800',
+  'stats_min_data_points'   => 5,
 
   // Uptime
-  'display_bitcoind_uptime'   => false,
-  'bitcoind_process_name'     => 'bitcoind',
+  'display_dogecoind_uptime' => true,
+  'dogecoind_process_name'   => 'dogecoind',
 
   // System
-  'date_format'               => 'H:i:s T, j F Y ',
-  'disk_space_mount_point'    => '.',
-  'timezone'                  => null,
-  'stylesheet'                => 'v2-light.css',
-  'debug'                     => false,
-  'admin_email'               => 'admin@example.com',
+  'date_format'             => 'H:i:s T, j F Y ',
+  'timezone'                => 'Europe/Paris',
+  'stylesheet'              => 'v2-dark.css',
+  'debug'                   => false,
+  'admin_email'             => 'empty',
 );
